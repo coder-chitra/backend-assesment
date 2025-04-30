4c8a308c29e0528769790d3d9380fff55b70a419
coder-chitra <chitra.shinde1220@gmail.com>
2025-04-30 16:29:22 +0530

    Auth Added

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Initializing Express App
 const express = require("express");
 const { Pool } = require("pg");
+// const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { get } = require("http");
+
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// const prisma = new PrismaClient();
 
 // creating db connection
 const pool = new Pool({
@@ -30,9 +37,16 @@ app.get("/register", (req, res) => {
 // resgistration route
 app.post("/register", async (req, res) => {
     const { username, email, password } = req.body;
+
+    // hasing of pass
+    // const hashedPassword = await bcrypt.hash(password, 10);
+    // const match = await bcrypt.compare(password, hashedPassword);
+    // const accessToken = jwt.sign(JSON.stringify(user), process.env.TOKEN_SECRET);
+    // console.log(match);
+    // console.log(hashedPassword);
+
+
     const insert_query = 'INSERT INTO users(username , email , password) VALUES ($1,$2,$3)';
-    console.log(insert_query);
-    console.log(username, email, password);
     try {
         await pool.query(insert_query, [username, email, password]);
         res.send("Data Inserted Successfully");
@@ -49,9 +63,9 @@ app.get("/login", (req, res) => {
 // login route
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    console.log(username, password);
-    const fetch_query = 'SELECT password FROM users WHERE username = $1';
+
     try {
+        const fetch_query = 'SELECT password FROM users WHERE username = $1';
         const result = await pool.query(fetch_query, [username]);
         if (result.rows.length == 0) {
             res.send("User doesn't exits , Register 1st and then login ");
@@ -59,12 +73,51 @@ app.post("/login", async (req, res) => {
         else if (result.rows[0].password === password) {
             res.redirect("/home/" + username);
         } else {
+
+            try {
+                // check if user id block or not
+                let block_query = `SELECT isblock from users where username= $1`;
+                let result = await pool.query(block_query, [username]);
+                if (result.rows[0].isblock == true) {
+                    console.log("User is Block");
+                }
+            } catch {
+                console.log("unable to get block status")
+            }
+
+            const getPasswordCnt = 'SELECT pswcnt FROM users WHERE username = $1';
+            // getting the password count 
+            try {
+                let cnt = await pool.query(getPasswordCnt, [username]);
+                let pswcnt = cnt.rows[0].pswcnt;
+                pswcnt++;
+                // updating count as attempt failed
+                if (pswcnt >= 5) {
+                    try {
+                        const update_query = `UPDATE users SET pswcnt = $1 where username = $2`;
+                        let updated = await pool.query(update_query, [pswcnt, username]);
+                    } catch (error) {
+                        console.log("Unable to update cnt");
+                    }
+                } else {
+                    try {
+                        const update_query = `UPDATE users SET pswcnt = $1 , isblock = true where username = $2;`;
+                        let updated = await pool.query(update_query, [pswcnt, username]);
+                    } catch (error) {
+                        console.log("Unable to update cnt");
+                    }
+                }
+
+            } catch (error) {
+                console.log("unable to get count");
+            }
             res.send("Wrong Password Entered");
         }
     } catch (error) {
         res.send("Unable to login");
     }
 });
+
 
 app.get("/home/:user", (req, res) => {
     let user = req.params.user;
